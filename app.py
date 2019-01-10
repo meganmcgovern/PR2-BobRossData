e96b631911d47459c0f2d12926812cc88ca1fc8a
Olivia <40842118+obnicholson@users.noreply.github.com>
2019-01-09 20:43:42 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 # Database Setup
 #################################################
